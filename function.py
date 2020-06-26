c82be1641547481d4e2a1825db5fe28374b6ac4b
lin1248 <1248134657@qq.com>
2020-06-26 12:04:12 +0800

    version 1.1
    Add the storeData function in demo again,optimized the program structure

--- function.py
+++ function.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import os
 
 def fetchURL(url):
     '''
@@ -27,17 +28,38 @@ def fetchURL(url):
     except:
         print("Unkown Error !")
 
-def getComments(text):
+def storeData(text,av,pageNum):
     '''
-    功能：处理json文件，只提取评论部分
+    功能：保存text到json文件中，以便后续处理
     参数：
-        filename，待处理文件的路径
+        text:网页返回的信息，str类型
+        av:视频av号，str类型
+        pageNum:评论的页数，str类型
+    返回：文件的路径，str类型
+    '''
+    # 判断是否存在data文件夹，否则新建
+    if not os.path.exists('data'):
+        os.mkdir('data')
+
+    filename = 'data\\' + pageNum + '_' + av + '.json'
+    data = json.loads(text)
+    with open(filename,'a',encoding='utf-8') as f:
+        f.write(json.dumps(data,indent=2,ensure_ascii=False))
+        print(pageNum+'done !')
+    return filename
+
+def getComments(text,av):
+    '''
+    功能：处理网页返回信息，只提取评论部分
+    参数：
+        text，待处理信息，str类型
+        av:视频av号，str类型
     '''
     dict = json.loads(text)
 
     replies = dict['data']['replies']
 
-    fw = open('comments.txt','a',encoding='utf-8')
+    fw = open(av + 'comments.txt','a',encoding='utf-8')
     while len(replies) > 0: 
         # 从replies提取出单条评论
         reply = replies.pop()  
@@ -47,6 +69,32 @@ def getComments(text):
         comments = reply['content']['message']
         # 评论发表的时间
         ctime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(reply['ctime']))
-        msg = 'bilibili users///  ' + uname + '///' + ctime + ':::' + '  say:  '+ comments
+        msg = 'bilibili users///' + uname + '///  ' + ctime + ':::' + '  say:  '+ comments
+        fw.write(msg+'\n')    
+    print('done !')
+
+def getComments2(filename,av):
+    '''
+    功能：提取本地文件中的用户评论
+    参数：
+        filename:待处理的本地文件路径，str类型
+        av:视频av号，str类型
+    '''
+    f = open(filename,'r',encoding='utf-8')
+    dict = json.load(f)
+    
+    replies = dict['data']['replies']
+
+    fw = open(av + 'comments.txt','a',encoding='utf-8')
+    while len(replies) > 0: 
+        # 从replies提取出单条评论
+        reply = replies.pop()  
+        # 发表评论的用户名称
+        uname = reply['member']['uname']
+        # 评论内容
+        comments = reply['content']['message']
+        # 评论发表的时间
+        ctime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(reply['ctime']))
+        msg = 'bilibili users///' + uname + '///  ' + ctime + ':::' + '  say:  '+ comments
         fw.write(msg+'\n')    
     print('done !')
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from function import fetchURL, getComments
+from function import fetchURL, getComments, storeData, getComments2
+from av import av_1, av_2, av_3
 '''
 入口
 '''
 
 if __name__ == "__main__":
-    av_1 = 286054084
-    av_2 = 583623415
+    # 在这统一修改av号
+    av = av_3
+
     api_url = 'https://api.bilibili.com/x/v2/reply?'
-    for i in range(1,230):
+    for i in range(1,474):
         # i为评论的页数范围
-        url = api_url + 'pn=' + str(i) + '&type=1&oid=' + str(av_2)
+        url = api_url + 'pn=' + str(i) + '&type=1&oid=' + str(av)
         text = fetchURL(url)
-        getComments(text)
+
+        # 方法1：直接提取评论
+        # getComments(text,str(av))
+
+        # 方法2：先保存在本地文件中在处理
+        filename = storeData(text,str(av),str(i))
+        getComments2(filename,str(av))
